63fcbdd008aae308cd056b75b272dbbffad41ee4
leandro <leandro.leal@southsystem.com.br>
2022-03-17 07:42:48 -0300

    Primeiro commit estrutura basica

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="logo" alt="Pergaminho Selado" src="./assets/selado.png">
+  <HelloWorld msg="Bem vindo ao Estudo do livro de Apocalipse"/>
 </template>
 
 <script>
@@ -16,11 +16,16 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif, ;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+}
+.logo{
+  height: 90%;
+  width: 90%;
+  
 }
 </style>
